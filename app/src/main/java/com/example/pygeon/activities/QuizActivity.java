c1b54c223ba967163136802bc51fc7236cf0ed90
Brett Kim <bkim2021@francisparker.org>
2020-07-18 22:49:09 -0700

    Quiz content for each lesson complete, quiz button centered, staged for graphics
    Co-authored-by: mellojello13 <lenaluostarinen@gmail.com>

--- app/src/main/java/com/example/pygeon/activities/QuizActivity.java
+++ app/src/main/java/com/example/pygeon/activities/QuizActivity.java
@@ -51,10 +51,10 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void loadQuestions(int i) {
-        if(enemyHP > 0) {
+        if (enemyHP > 0) {
             //Loads the new question here with the correct data
             final Question quest = questionList.get(i);
-            tvQuizCounter.setText(enemyHP + "/" + maxEnemyHP + " HP");
+            tvQuizCounter.setText("Minotaur: " + enemyHP + "/" + maxEnemyHP + " HP");
             tvQuizQuestion.setText(quest.getQuestion());
             buttonA.setText(quest.getA());
             buttonB.setText(quest.getB());
@@ -126,8 +126,8 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void takeDamage() {
-        if(enemyHP != 0) {
-            tvQuizCounter.setText(enemyHP + "/" + maxEnemyHP + " HP");
+        if (enemyHP != 0) {
+            tvQuizCounter.setText("Minotaur: " + enemyHP + "/" + maxEnemyHP + " HP");
             tvQuizStatus.setTextColor(Color.parseColor("#3dcc8e"));
             tvQuizStatus.setText("Correct! Enemy took 3 damage.");
             enemyHP -= 3;
@@ -135,8 +135,8 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void healDamage() {
-        if(enemyHP != 0) {
-            tvQuizCounter.setText(enemyHP + "/" + maxEnemyHP + " HP");
+        if (enemyHP != 0) {
+            tvQuizCounter.setText("Minotaur: " + enemyHP + "/" + maxEnemyHP + " HP");
             tvQuizStatus.setTextColor(Color.parseColor("#f44336"));
             tvQuizStatus.setText("Incorrect! Enemy healed 2 damage.");
             enemyHP += 2;
@@ -145,7 +145,7 @@ public class QuizActivity extends AppCompatActivity {
 
     public void enemyDefeated() {
         enemyHP = 0;
-        tvQuizCounter.setText(enemyHP + "/" + maxEnemyHP + " HP");
+        tvQuizCounter.setText("Minotaur: " + enemyHP + "/" + maxEnemyHP + " HP");
         tvQuizStatus.setText("You win!");
 
         /*
@@ -154,50 +154,65 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void load(int i) {
-        /*
-         * NEED TO UPDATE THIS
-         */
+        // Level One: Variables
+        questionList.clear();
 
-        if (i == 1) {
-            // Level One: Variables
-            questionList.clear();
-            questionList.add(new Question("What is a valid variable name in Python?", "dog$", "cats", "100dogs", "several-cats", "cats"));
-            questionList.add(new Question("What data type is person = \"Mama\"", "String", "int", "double", "boolean", "String"));
-            questionList.add(new Question("What data type is apples = 4", "String", "int", "double", "boolean", "int"));
-            questionList.add(new Question("What data type is score = 5.6", "String", "int", "double", "boolean", "double"));
-            questionList.add(new Question("What data type is alive = true", "String", "int", "double", "boolean", "boolean"));
-        } else if (i == 2) {
-            // Level Two: Syntax
-            questionList.clear();
-            questionList.add(new Question("What is a valid variable name in Python?", "dog$", "cats", "100dogs", "several-cats", "cats"));
-            questionList.add(new Question("What data type is person = \"Mama\"", "String", "int", "double", "boolean", "String"));
-            questionList.add(new Question("What data type is apples = 4", "String", "int", "double", "boolean", "int"));
-            questionList.add(new Question("What data type is score = 5.6", "String", "int", "double", "boolean", "double"));
-            questionList.add(new Question("What data type is alive = true", "String", "int", "double", "boolean", "boolean"));
-        } else if (i == 3) {
-            // Level Three: Control Structures
-            questionList.clear();
-            questionList.add(new Question("What is a valid variable name in Python?", "dog$", "cats", "100dogs", "several-cats", "cats"));
-            questionList.add(new Question("What data type is person = \"Mama\"", "String", "int", "double", "boolean", "String"));
-            questionList.add(new Question("What data type is apples = 4", "String", "int", "double", "boolean", "int"));
-            questionList.add(new Question("What data type is score = 5.6", "String", "int", "double", "boolean", "double"));
-            questionList.add(new Question("What data type is alive = true", "String", "int", "double", "boolean", "boolean"));
-        } else if (i == 4) {
-            // Level Four: Arrays
-            questionList.clear();
-            questionList.add(new Question("What is a valid variable name in Python?", "dog$", "cats", "100dogs", "several-cats", "cats"));
-            questionList.add(new Question("What data type is person = \"Mama\"", "String", "int", "double", "boolean", "String"));
-            questionList.add(new Question("What data type is apples = 4", "String", "int", "double", "boolean", "int"));
-            questionList.add(new Question("What data type is score = 5.6", "String", "int", "double", "boolean", "double"));
-            questionList.add(new Question("What data type is alive = true", "String", "int", "double", "boolean", "boolean"));
-        } else if (i == 5) {
-            // Level Five: Loops
-            questionList.clear();
-            questionList.add(new Question("What is a valid variable name in Python?", "dog$", "cats", "100dogs", "several-cats", "cats"));
-            questionList.add(new Question("What data type is person = \"Mama\"", "String", "int", "double", "boolean", "String"));
-            questionList.add(new Question("What data type is apples = 4", "String", "int", "double", "boolean", "int"));
-            questionList.add(new Question("What data type is score = 5.6", "String", "int", "double", "boolean", "double"));
-            questionList.add(new Question("What data type is alive = true", "String", "int", "double", "boolean", "boolean"));
+        if(i == 1) {
+            questionList.add(new Question("What type is the variable answer? answer = \"B\"", "String", "int", "double", "boolean", "String"));
+            questionList.add(new Question("What type is the variable num_cats? num_cats = 4", "String", "int", "double", "boolean", "int"));
+            questionList.add(new Question("What type is the variable cash? cash = 4.35", "String", "int", "double", "boolean", "double"));
+            questionList.add(new Question("What type is the variable alive? alive = True", "String", "int", "double", "boolean", "boolean"));
+            questionList.add(new Question("Which math operations can you use with Python?", "Addition (+)", "Subtraction (-)", "Multiplication (*)", "All of the above and more!", "All of the above and more!"));
+            questionList.add(new Question("How do you print things to the console in Python?", "print = \"Fruit\"", "sole(\"Fruit\")", "print(\"Fruit\")", "console.Fruit", "print(\"Fruit\")"));
+            questionList.add(new Question("What is a valid variable name in Python?", "23dogs", "dog$", "-dogs", "dogs", "dogs"));
+            questionList.add(new Question("What is the parameter in this print statement? print(\"Cake\")", "\"Cake\"", "print", "(\"\")", "Cake", "\"Cake\""));
+            questionList.add(new Question("What type is the variable fruit? fruit = [\"Apple\", \"Orange\", \"Banana\"]", "int", "double", "list", "boolean", "list"));
+            questionList.add(new Question("What is the type of the parameter? wait(\"200\")", "String", "int", "double", "boolean", "String"));
+        } else if(i == 2) {
+            questionList.add(new Question("Which variable is correctly defined?", "buttonPressed.True", "True buttonPressed", "buttonPressed = True", "True = buttonPressed", "buttonPressed = True"));
+            questionList.add(new Question("Which comment is correctly defined?", "# This is a comment", "// This is a comment", "\"This is a comment\"", "This is a comment", "# This is a comment"));
+            questionList.add(new Question("What is the name of this variable? age = 14", "age = 14", "age", "age14", "14", "age"));
+            questionList.add(new Question("What is the value of this variable? age = 14", "age = 14", "age", "age14", "14", "14"));
+            questionList.add(new Question("What naming convention does Python recommend?", "Upper camel case (VarName)", "Camel case (varName)", "Snake case (var_name)", "Kebab Case (var-name)", "Snake case (var_name)"));
+            questionList.add(new Question("What variable name is a reserved keyboard and cannot be used as a variable name?", "rpm", "double", "num_cats", "numberCats", "double"));
+            questionList.add(new Question("How do you call the startswith function from String s?", "startswith(s)", "startswith.String", "s.startsWith()", "s.startsWith", "s.startsWith()"));
+            questionList.add(new Question("What is a parameter?", "A unit of measurement", "A given value to a function", "A data type", "A function", "A given value to a function"));
+            questionList.add(new Question("What is the proper syntax to call the print function?", "print()", "print{}", "print[]", "print", "print()"));
+            questionList.add(new Question("Say name() returns a String value. What line of code sets name() to a variable x?", "x.name()", "x = name()", "name(x)", "x = name", "x = name()"));
+        } else if(i == 3) {
+            questionList.add(new Question("What data type can be True or False?", "String", "int", "double", "boolean", "boolean"));
+            questionList.add(new Question("Which pair contains both boolean operators?", "True, or", "and, False", "True, False", "and, or", "and, or"));
+            questionList.add(new Question("Which pair contains both boolean values?", "True, or", "and, False", "True, False", "and, or", "True, False"));
+            questionList.add(new Question("What does (isFun and isPlayable) evaluate to if isFun = True and isPlayable = False)?", "True", "False", "and", "None of the above", "False"));
+            questionList.add(new Question("What does (isFun or isPlayable) evaluate to if isFun = True and isPlayable = False)?", "True", "False", "and", "None of the above", "True"));
+            questionList.add(new Question("What will print to the console from if isFun: print(“It’s fun”) when isFun = True?", "It’s fun", "\"It's\"fun", "print(“It’s fun”)", "Nothing", "It’s fun"));
+            questionList.add(new Question("What will print to the console from if num_cakes == 5: num_cakes == 6 if num_cakes == 6: print(“Six cakes”), when num_cakes = 5?", "Six cakes", "True", "False", "Nothing will print", "Six cakes"));
+            questionList.add(new Question("What will print to the console from if num_cakes == 5: num_cakes == 6 elif num_cakes == 6: print(“Six cakes”), when num_cakes = 5?", "Six cakes", "True", "False", "Nothing will print", "Nothing will print"));
+            questionList.add(new Question("What will print to the console from if isRed: print(“It’s red”) else: print(“It’s not red”), when isRed = True?", "It's red", "It's not red", "\"It's red\"", "\"It's not red\"", "It's red"));
+            questionList.add(new Question("What will print to the console from if isRed: print(“It’s red”) else: print(“It’s not red”), when isRed = False?", "It's red", "It's not red", "\"It's red\"", "\"It's not red\"", "It's not red"));
+        } else if(i == 4) {
+            questionList.add(new Question("Which points list is properly defined?", "points = {33.3, 45.6, 35.5, 0.14}", "points = 33.3, 45.6, 35.5, 0.49", "points = [23.3, 41.5, 6.53, 0.84]", "points [23.3, 41.5, 6.53, 0.84]", "points = [23.3, 41.5, 6.53, 0.84]"));
+            questionList.add(new Question("Select the first element of prices = [23.3, 41.5, 6.53, 0.84]", "prices[0]", "prices[1]", "prices[2]", "prices[23.3]", "prices[0]"));
+            questionList.add(new Question("Select the second element of prices = [23.3, 41.5, 6.53, 0.84]", "prices[0]", "prices[1]", "prices[2]", "prices[23.3]", "prices[1]"));
+            questionList.add(new Question("What is the 1 in pets[1] called?", "A position", "A number", "An index", "A spot", "An index"));
+            questionList.add(new Question("prices is a list that contains 4. How can you find the index of 4?", "prices[3]", "prices[4]", "prices.index(3)", "prices.index(4)", "prices.index(4)"));
+            questionList.add(new Question("How would you add the number 6 to the end of the prices list?", "prices.add(6)", "prices.append(6)", "prices[6]", "append(6)", "prices.append(6)"));
+            questionList.add(new Question("How would you sort a list of numbers named prices from least to greatest?", "prices.sort()", "sort(prices)", "prices.sort", "prices[]", "prices.sort()"));
+            questionList.add(new Question("How would you remove the 2nd element, the number 34, of the list numbers?", "numbers.pop(1)", "numbers.pop(2)", "numbers.pop(34)", "numbers.remove(34)", "numbers.pop(1)"));
+            questionList.add(new Question("How would you insert the string “Gold” at index 2 in the list chemistry?", "chemistry.insert(1, “Gold”)", "chemistry.insert(2, “Gold”)", "chemistry.insert(“Gold”, 1)", "chemistry.insert(“Gold”, 2)", "chemistry.insert(2, “Gold”)"));
+            questionList.add(new Question("In the list points [23.3, 41.5, 6.53, 0.84], what is the position and index of 0.84?", "Position: 2, Index: 2", "Position: 3, Index: 3", "Position: 2, Index: 3", "Position: 3, Index: 2", "Position: 3, Index: 2"));
+        } else if(i == 5) {
+            questionList.add(new Question("What is the object in (for book in books)?", "for", "book", "in", "books", "book"));
+            questionList.add(new Question("When will a while loop stop?", "When its condition is True", "When its condition is False", "Never, a while loop runs constantly", "None of the above", "When its condition is False"));
+            questionList.add(new Question("How do you loop through a 5-element list numbers by object?", "for numbers", "for number in numbers", "for numbers in numbers", "for i in range(0, 5)", "for number in numbers"));
+            questionList.add(new Question("How do you loop through a 5-element list numbers using a numerical index?", "for numbers", "for number in numbers", "for numbers in numbers", "for i in range(0, 5)", "for i in range(0, 5)"));
+            questionList.add(new Question("Does the index variable in a numerical index for loop need to be i?", "Yes, Python requires it", "Sometimes, only with lists", "Sometimes, only with indexes", "No, the index variable can vary in name", "No, the index variable can vary in name"));
+            questionList.add(new Question("How many times will for i in range(4, 8): print(“Banana”) print Banana?", "3", "4", "5", "8", "4"));
+            questionList.add(new Question("Can if and else loops exist inside of for loops?", "Yes", "Sometimes, only with integers", "Sometimes, only with Strings", "Never", "Yes"));
+            questionList.add(new Question("When looping through a loop, is a for loop or while loop easier to implement?", "For loop, because the code is longer", "For loop, because the code is shorter", "While loop, because the code is longer", "While loop, because the code is shorter", "For loop, because the code is shorter"));
+            questionList.add(new Question("Can we track position with a for loop that cycles through objects in a list?", "Yes, always", "Sometimes at random", "Not by itself, but with an incrementing index", "No", "Not by itself, but with an incrementing index"));
+            questionList.add(new Question("What is the list in (for book in books)?", "for", "book", "in", "books", "books"));
         }
+
     }
 }
